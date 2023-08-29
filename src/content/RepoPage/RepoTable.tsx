b1f885c878fe7edca408e8818d79f7ded4f3379b
Maycon Belfort <maycon.belfort@gmail.com>
2023-08-29 20:25:59 +1000

    feat(tutorial): complete step 3

--- src/content/RepoPage/RepoTable.tsx
+++ src/content/RepoPage/RepoTable.tsx
@@ -14,6 +14,10 @@ import {
 } from '@carbon/react';
 
 const RepoTable = ({ rows, headers }) => {
+  const getRowDescription = (rowId) => {
+    const row = rows.find(({ id }) => id === rowId);
+    return row ? row.description : '';
+  };
   return (
     <DataTable
       rows={rows}
@@ -38,7 +42,7 @@ const RepoTable = ({ rows, headers }) => {
                     ))}
                   </TableExpandRow>
                   <TableExpandedRow colSpan={headers.length + 1}>
-                    <p>Row description</p>
+                    <p>{getRowDescription(row.id)}</p>
                   </TableExpandedRow>
                 </React.Fragment>
               ))}
